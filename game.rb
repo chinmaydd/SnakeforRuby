810bf4ac202161f92609cc49e110a313b7d6a74d
Chinmay <chinmay_dd@yahoo.co.in>
2014-05-29 22:17:17 +0530

    class snake added

--- game.rb
+++ game.rb
@@ -1,1 +1,32 @@
-#this is for the snake game
+class Snake
+    @turn_pts_x = [50]
+    @turn_pts_y = [50]
+    @dir = 'u'
+    @head_x = 50 
+    @head_y = 50 
+    @tail_x = 50
+    @tail_y = 51
+    @flag1 = false
+    @flag2 = false
+    def update_turn
+       turn_pts_x << @head_x
+       turn_pts_y << @head_y
+       end
+    def turn_right                                                                                        
+        dir = 'r'
+        update_turn                                                                   
+        end
+    def go_up                                   
+        dir = 'u'
+        update_turn
+        end
+    def go_down
+        dir = 'd'
+        update_turn
+        end
+    def turn_left
+        dir = 'l'
+        update_turn      
+        end
+    
+end
